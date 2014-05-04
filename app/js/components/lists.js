639d5d2f907ffdeaa74827b64562a658fc620af1
chiayenhung <hodalalala@gmail.com>
2014-05-04 14:26:38 -0400

    almost done

--- app/js/components/lists.js
+++ app/js/components/lists.js
@@ -2,7 +2,7 @@
 var generateList = function () {
   var $buildingList = $(".building_list");
   $buildingList.empty();
-  buildings.sortBy().data.forEach(function(building, index){
+  buildings.data.forEach(function(building, index){
     var content = _.template(JST['buildingsList'], building);
     $buildingList.append(content);
   });
